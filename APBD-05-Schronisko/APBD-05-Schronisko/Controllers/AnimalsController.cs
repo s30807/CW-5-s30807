7fd181d4e24f88fb4e45be4472bca272d46273d8
s30807 <s30807@pjwstk.edu.pl>
2025-04-14 00:29:19 +0200

    Version 1. Not finished.

---
+++ APBD-05-Schronisko/APBD-05-Schronisko/Controllers/AnimalsController.cs
@@ -0,0 +1,96 @@
+﻿using APBD_05_Schronisko.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APBD_05_Schronisko.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AnimalsController : ControllerBase
+{
+    public static List<Animal> Animals = [
+    new Animal()
+    {
+        Id = 1,
+        Category = "Cats",
+        Color = "Blue",
+        Name = "Cat",
+        Mass = 15.0,
+    },
+    new Animal()
+    {
+        Id = 2,
+        Category = "Dogs",
+        Color = "Red",
+        Name = "Dog",
+        Mass = 17.0,
+    }];
+
+    //1
+    [HttpGet]
+    public IActionResult GetAllAnimals()
+    {
+        return Ok(Animals);
+    }
+    
+    //2
+    [HttpGet]
+    [Route("{id}")]
+    public IActionResult GetAnimalById(int id)
+    {
+        var animal = Animals.FirstOrDefault(e => e.Id == id);
+
+        if (animal == null)
+        {
+            return NotFound($"Animal with id: {id} not found");
+        }
+    
+        return Ok(animal);
+    }
+    
+    //3
+    [HttpPost]
+    public IActionResult AddAnimal(Animal animal)
+    {
+        var nextId = Animals.Max(e => e.Id) + 1;
+        
+        animal.Id = nextId;
+        Animals.Add(animal);
+        
+        return CreatedAtAction(nameof(GetAnimalById), new { id = animal.Id }, animal);
+    }
+    
+    //4
+    [HttpPut]
+    [Route("{id}")]
+    public IActionResult ReplaceStudent(int id, Animal animal)
+    {
+        var animalToUpdate = Animals.FirstOrDefault(e => e.Id == id);
+
+        if (animalToUpdate == null)
+        {
+            return NotFound($"Animal with id: {id} not found");
+        }
+        
+        animalToUpdate.Name = animal.Name;
+        animalToUpdate.Category = animal.Category;
+        animalToUpdate.Mass = animal.Mass;
+        animalToUpdate.Color = animal.Color;
+        
+        return NoContent();
+    }
+    
+    //5
+    [HttpDelete]
+    public IActionResult DeleteAnimal(int id)
+    {
+        var animalToDelete = Animals.FirstOrDefault(e => e.Id == id);
+
+        if (animalToDelete == null)
+        {
+            return NotFound($"Animal with id: {id} not found");
+        }
+        Animals.Remove(animalToDelete);
+        
+        return NoContent();
+    }
+}
---
+++ APBD-05-Schronisko/APBD-05-Schronisko/Models/Animal.cs
@@ -0,0 +1,10 @@
+﻿namespace APBD_05_Schronisko.Models;
+
+public class Animal
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public double Mass { get; set; }
+    public string Color { get; set; }
+}
